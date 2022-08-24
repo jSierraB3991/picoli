127e318777c0e86abb212385855a35e6e6cd08b8
Juan David Sierra <judas3991@gmail.com>
2022-08-23 19:34:11 -0500

    add lombok, to todo-cli

--- todo-cli/src/main/java/com/mycompany/app/models/Todo.java
+++ todo-cli/src/main/java/com/mycompany/app/models/Todo.java
@@ -3,6 +3,20 @@ package com.mycompany.app.models;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class Todo implements Serializable {
 
     private static final long serialVersionUID = 4663044935291053899L;
@@ -12,52 +26,4 @@ public class Todo implements Serializable {
     private Status status;
     private LocalDate createOn;
     private LocalDate updateOn;
-
-    public Todo() {
-      super();
-    }
-
-    public Todo(Long id, String message, LocalDate createOn, LocalDate updateOn) {
-      super();
-      this.id = id;
-      this.message = message;
-      this.status = status;
-      this.createOn = createOn;
-      this.updateOn = updateOn;
-    }
-
-    public Long getId() {
-      return this.id;
-    }
-    public void setId(Long id) {
-      this.id = id;
-    }
-
-    public String getMessage() {
-      return this.message;
-    }
-    public void setMessage(String message) {
-      this.message = message;
-    }
-
-    public Status getStatus() {
-      return this.status;
-    }
-    public void setStatus(Status status) {
-      this.status = status;
-    }
-
-    public LocalDate getCreateOn() {
-      return this.createOn;
-    }
-    public void setCreateOn(LocalDate createOn) {
-      this.createOn = createOn;
-    }
-
-    public LocalDate getUpdateOn() {
-      return this.createOn;
-    }
-    public void setUpdateOn(LocalDate updateOn) {
-      this.updateOn = updateOn;
-    }
 }
