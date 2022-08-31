a093b85462994c3b3e0828e5e023c25e72c911e2
Juan David Sierra <judas3991@gmail.com>
2022-08-31 01:18:30 -0500

    change var for obviusly type

--- load-data-cli/src/main/java/com/mycompany/app/commands/LoadCommand.java
+++ load-data-cli/src/main/java/com/mycompany/app/commands/LoadCommand.java
@@ -10,11 +10,9 @@ import me.tongfei.progressbar.ProgressBarBuilder;
 import me.tongfei.progressbar.ProgressBarStyle;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
 import picocli.CommandLine;
 
 import java.io.IOException;
-import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -52,41 +50,40 @@ public class LoadCommand implements Runnable {
         var folders = stringFile.split("/");
         var fileName = folders[folders.length - 1];
 
-        List<ClientRequest> collection = getData(stringFile);
-        ProgressBarBuilder pbb = new ProgressBarBuilder()
+        var collection = getData(stringFile);
+        var pbb = new ProgressBarBuilder()
                 .setStyle(ProgressBarStyle.UNICODE_BLOCK)
                 .setTaskName(fileName + ":")
                 .showSpeed();
-        for (ClientRequest clientRequest : ProgressBar.wrap(collection, pbb)) {
+        for (var clientRequest : ProgressBar.wrap(collection, pbb)) {
             //TODO REMOVE MESSAGE FOR HIBERNATE
             //service.saveClient(clientRequest);
             TimeUnit.MILLISECONDS.sleep(50);
         }
-        System.out.println(new ConsoleColors(ConsoleColors.TEXT_GREEN, ConsoleColors.TEXT_BG_BLACK, "Finish migrations for " + stringFile).getColoredString());
+        var colorConsole = new ConsoleColors(ConsoleColors.TEXT_GREEN, ConsoleColors.TEXT_BG_BLACK, "Finish migrations for " + stringFile);
+        System.out.println(colorConsole.getColoredString());
         System.out.println();
     }
 
     private List<ClientRequest> getData(String stringFile) {
-
         String[] header = {"name", "email", "address", "region", "country", "text", "numberrange"};
-        List<ClientRequest> requests = new ArrayList<>();
+        var requests = new ArrayList<ClientRequest>();
         try (
-                Reader reader = Files.newBufferedReader(Paths.get(stringFile));
-                CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
+                var reader = Files.newBufferedReader(Paths.get(stringFile));
+                var csvParser = new CSVParser(reader, CSVFormat.DEFAULT
                         .withHeader(header)
                         .withFirstRecordAsHeader()
                         .withIgnoreHeaderCase()
                         .withTrim());
         ) {
-            for (CSVRecord csvRecord : csvParser) {
-                String name = csvRecord.get(header[0]);
-                String email = csvRecord.get(header[1]);
-                String address = csvRecord.get(header[2]);
-                String region = csvRecord.get(header[3]);
-                String country = csvRecord.get(header[4]);
-                String text = csvRecord.get(header[5]);
-                String number = csvRecord.get(header[6]);
-
+            for (var csvRecord : csvParser) {
+                var name = csvRecord.get(header[0]);
+                var email = csvRecord.get(header[1]);
+                var address = csvRecord.get(header[2]);
+                var region = csvRecord.get(header[3]);
+                var country = csvRecord.get(header[4]);
+                var text = csvRecord.get(header[5]);
+                var number = csvRecord.get(header[6]);
                 requests.add(ClientRequest.builder()
                         .name(name)
                         .email(email)
@@ -100,7 +97,6 @@ public class LoadCommand implements Runnable {
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
-
         return requests;
     }
 }
