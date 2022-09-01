ef6a9411d14c9252440fdf6dd0fcfa418eff0444
Juan David Sierra <judas3991@gmail.com>
2022-09-01 00:35:10 -0500

    optimize read csv files

--- load-data-cli/src/main/java/com/mycompany/app/utils/impl/FileProcessorImpl.java
+++ load-data-cli/src/main/java/com/mycompany/app/utils/impl/FileProcessorImpl.java
@@ -4,6 +4,7 @@ import com.mycompany.app.response.ClientRequest;
 import com.mycompany.app.utils.FileProcessor;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,9 +17,7 @@ public class FileProcessorImpl implements FileProcessor {
 
     public List<ClientRequest> fileProcessOfClient(String stringFile) {
         String[] header = {"name", "email", "address", "region", "country", "text", "numberrange"};
-        return fileProcess(header, stringFile, (csvParser) -> {
-            var response = new ArrayList<ClientRequest>();
-            for (var csvRecord : csvParser) {
+        return fileProcess(header, stringFile, (csvRecord) -> {
                 var name = csvRecord.get(header[0]);
                 var email = csvRecord.get(header[1]);
                 var address = csvRecord.get(header[2]);
@@ -26,7 +25,7 @@ public class FileProcessorImpl implements FileProcessor {
                 var country = csvRecord.get(header[4]);
                 var text = csvRecord.get(header[5]);
                 var number = csvRecord.get(header[6]);
-                response.add(ClientRequest.builder()
+                return ClientRequest.builder()
                         .name(name)
                         .email(email)
                         .address(address)
@@ -34,14 +33,12 @@ public class FileProcessorImpl implements FileProcessor {
                         .country(country)
                         .aleatoryText(text)
                         .aleatoryNumber(Integer.parseInt(number))
-                        .build());
-            }
-            return response;
+                        .build();
         });
     }
 
-    private <T> List<T> fileProcess(String[] headers, String fileName, Function<CSVParser, ArrayList<T>> func ) {
-         var requests = new ArrayList<T>();
+    private <T> List<T> fileProcess(String[] headers, String fileName, Function<CSVRecord, T> funcIterable) {
+        var requests = new ArrayList<T>();
         try (
                 var reader = Files.newBufferedReader(Paths.get(fileName));
                 var csvParser = new CSVParser(reader, CSVFormat.DEFAULT
@@ -51,7 +48,9 @@ public class FileProcessorImpl implements FileProcessor {
                         .withTrim())
         ) {
 
-            requests = func.apply(csvParser);
+            for (var record : csvParser) {
+                requests.add(funcIterable.apply(record));
+            }
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
