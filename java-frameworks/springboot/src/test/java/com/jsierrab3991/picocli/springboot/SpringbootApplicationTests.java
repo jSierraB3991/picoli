a2c2423e913e509a552bafde223243435d772ea8
Juan David Sierra <juan.sierra@zabud.com.co>
2022-10-26 20:21:30 -0500

    add project for picocli with spring boot

---
+++ java-frameworks/springboot/src/test/java/com/jsierrab3991/picocli/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsierrab3991.picocli.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
